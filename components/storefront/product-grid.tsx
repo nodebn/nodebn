d463e9b2c6b03852a4e5d385f6ddbcbe02069c2e
nodebn <nodebrunei@gmail.com>
2026-04-06 21:43:45 +0800

    Storefront update

--- components/storefront/product-grid.tsx
+++ components/storefront/product-grid.tsx
@@ -21,6 +21,14 @@ import { Label } from "@/components/ui/label";
 import { formatMoney } from "@/lib/format";
 import { slugify } from "@/lib/slugify";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import { cn } from "@/lib/utils";
 
 export type StorefrontProduct = {
